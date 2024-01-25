a07f3da71c7a74d03d29a6d18d49ff401cb8f473
Mr-Bubble <314877984@qq.com>
2024-01-25 09:27:35 +0000

    Deploying to gh-pages from @ Mr-Bubble/versa_kit@889c0e8c86ce01cee16e457c0b12674ad934910d 🚀

---
+++ static/js/index-3763ab1f.js
@@ -0,0 +1,1 @@
+import{h as t}from"./index-5c8662dc.js";const i="//tenapi.cn";function n(e){return t.request({url:"/v2/images",method:"get",params:e,individuation:{app_api:i}})}function p(e){return t.request({url:"/v2/video",method:"get",params:e,individuation:{app_api:i}})}export{p as a,n as g};
--- static/js/index-6ded43aa.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./index-e81875e3.js";const o={};function r(t,s){return c(),n("div",null,"这个也啥都没有")}const a=e(o,[["render",r]]);export{a as default};
