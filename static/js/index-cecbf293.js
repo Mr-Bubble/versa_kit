42a1afcf714f9579ff0b2fe7d3be946b5236efc7
Mr-Bubble <314877984@qq.com>
2024-02-01 08:53:31 +0000

    Deploying to gh-pages from @ Mr-Bubble/versa_kit@aab1d2cf074e0c3f3cda93ccb54b144709ab7363 🚀

---
+++ static/js/index-cecbf293.js
@@ -0,0 +1,1 @@
+import{_ as o,o as r,c as t,a as c}from"./index-e22f5d96.js";import{E as n}from"./index-6ce67061.js";import"./use-id-60c7bb0d.js";const a={};function s(_,m){const e=n;return r(),t("div",null,[c(e,{image:"error",description:"这个也啥都没有"})])}const f=o(a,[["render",s]]);export{f as default};
---
+++ static/js/use-id-60c7bb0d.js
@@ -0,0 +1,1 @@
+import{av as e}from"./index-e22f5d96.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return`${t}-${++r}`}export{u};
