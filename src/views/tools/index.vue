374520453fac7ef0848c4c52dfa517eebd96f139
Bubble <bubble@aratum.cn>
2024-02-18 17:43:34 +0800

    feat: 大唐不夜城节目表

--- src/views/tools/index.vue
+++ src/views/tools/index.vue
@@ -2,6 +2,417 @@
 
 <template>
   <div>
-    <van-empty image="error" description="啥都没有" />
+    <table
+      border="2"
+      cellpadding="0"
+      cellspacing="0"
+      height="1350"
+      class=""
+      align="center"
+      bordercolor="#000000"
+      style="width: 600px"
+    >
+      <tbody>
+        <tr>
+          <td
+            class="et2"
+            height="30"
+            width="81"
+            style="
+              text-align: center;
+              width: 81px;
+              height: 30px;
+              background-color: #dfc5a4;
+            "
+          >
+            <span class="font1"><strong>分类</strong></span>
+          </td>
+          <td
+            class="et2"
+            width="201"
+            style="text-align: center; width: 201px; background-color: #dfc5a4"
+          >
+            <span class="font1"><strong>节目名称</strong></span>
+          </td>
+          <td
+            class="et2"
+            width="197"
+            style="text-align: center; width: 197px; background-color: #dfc5a4"
+          >
+            <span class="font1"><strong>演出时间</strong></span>
+          </td>
+          <td
+            class="et2"
+            width="232"
+            style="text-align: center; width: 232px; background-color: #dfc5a4"
+          >
+            <span class="font1"><strong>演出区域</strong></span>
+          </td>
+        </tr>
+        <tr>
+          <td
+            rowspan="10"
+            class="et3"
+            height="300"
+            width="81"
+            style="text-align: center"
+          >
+            <span class="font2">大唐演绎</span>
+          </td>
+          <td rowspan="2" class="et3" width="201" style="text-align: center">
+            <span class="font2">诗歌艺术</span>
+          </td>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">19:00-19:10</span>
+          </td>
+          <td rowspan="2" class="et3" width="232" style="text-align: center">
+            <span class="font2">诗歌雕塑群内</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">19:30-19:40</span>
+          </td>
+        </tr>
+        <tr>
+          <td rowspan="2" class="et3" width="201" style="text-align: center">
+            <span class="font2">花车斗彩</span>
+          </td>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">19:15-19:25</span>
+          </td>
+          <td rowspan="2" class="et3" width="232" style="text-align: center">
+            <span class="font2">团花广场</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">19:40-19:50</span>
+          </td>
+        </tr>
+        <tr>
+          <td rowspan="2" class="et3" width="201" style="text-align: center">
+            <span class="font2">贞观之治</span>
+          </td>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">19:20-19:30</span>
+          </td>
+          <td rowspan="2" class="et3" width="232" style="text-align: center">
+            <span class="font2">贞观之治雕塑群南侧</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">20:00-20:10</span>
+          </td>
+        </tr>
+        <tr>
+          <td rowspan="2" class="et3" width="201" style="text-align: center">
+            <span class="font2">戏演壁画</span>
+          </td>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">19:00-19:10</span>
+          </td>
+          <td rowspan="2" class="et3" width="232" style="text-align: center">
+            <span class="font2">大唐文化柱</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">19:40-19:50</span>
+          </td>
+        </tr>
+        <tr>
+          <td rowspan="2" class="et3" width="201" style="text-align: center">
+            <span class="font2">霓裳羽衣</span>
+          </td>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">19:30-19:40</span>
+          </td>
+          <td rowspan="2" class="et3" width="232" style="text-align: center">
+            <span class="font2">开元广场北侧</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">20:00-20:10</span>
+          </td>
+        </tr>
+        <tr>
+          <td
+            rowspan="4"
+            class="et4"
+            height="120"
+            width="81"
+            style="text-align: center"
+          >
+            <span class="font2">主题演出</span>
+          </td>
+          <td rowspan="4" class="et3" width="201" style="text-align: center">
+            <span class="font2">大雁塔水舞灯光秀</span>
+          </td>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">12:00-12:10</span>
+          </td>
+          <td rowspan="4" class="et3" width="232" style="text-align: center">
+            <span class="font2">大雁塔北广场</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">16:00-16:10</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">19:00-19:10</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">21:00-21:10</span>
+          </td>
+        </tr>
+        <tr>
+          <td
+            rowspan="18"
+            class="et3"
+            height="540"
+            width="81"
+            style="text-align: center"
+          >
+            <span class="font2">行为艺术</span>
+          </td>
+          <td rowspan="3" class="et4" width="201">
+            <div style="text-align: center">开元广场不倒翁</div>
+            <div style="text-align: center">北广场不倒翁</div>
+          </td>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">19:30-19:45</span>
+          </td>
+          <td rowspan="3" class="et4" width="232">
+            <div style="text-align: center">开元广场东侧</div>
+            <div style="text-align: center">大雁塔北广场西辅门</div>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">20:15-20:25</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">21:30-21:45</span>
+          </td>
+        </tr>
+        <tr>
+          <td rowspan="4" class="et3" width="201" style="text-align: center">
+            <span class="font2">盛唐密盒</span>
+          </td>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">19:10-19:20</span>
+          </td>
+          <td rowspan="4" class="et3" width="232" style="text-align: center">
+            <span class="font2">老字号德发长西侧</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">20:00-20:10</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">21:00-21:10</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">22:00-22:10</span>
+          </td>
+        </tr>
+        <tr>
+          <td rowspan="4" class="et3" width="201" style="text-align: center">
+            <span class="font2">旋转的胡旋</span>
+          </td>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">19:10-19:20</span>
+          </td>
+          <td rowspan="4" class="et3" width="232" style="text-align: center">
+            <span class="font2">唐食坊入口</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">20:30-20:40</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">21:30-21:40</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">22:30-22:40</span>
+          </td>
+        </tr>
+        <tr>
+          <td rowspan="3" class="et3" width="201" style="text-align: center">
+            <span class="font2">乐舞长安</span>
+          </td>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">19:00-19:10</span>
+          </td>
+          <td rowspan="3" class="et3" width="232" style="text-align: center">
+            <span class="font2">美术馆与影城中央</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">20:20-20:30&nbsp;</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">22:20-22:30</span>
+          </td>
+        </tr>
+        <tr>
+          <td rowspan="4" class="et3" width="201" style="text-align: center">
+            <span class="font2">华灯太白</span>
+          </td>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">19:40-19:50</span>
+          </td>
+          <td rowspan="4" class="et3" width="232" style="text-align: center">
+            <span class="font2">音乐厅与大剧院中央</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">20:50-21:00</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">21:40-21:50</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">22:40-22:50</span>
+          </td>
+        </tr>
+        <tr>
+          <td
+            rowspan="10"
+            class="et3"
+            height="300"
+            width="81"
+            style="text-align: center"
+          >
+            <span class="font2">音乐舞台</span>
+          </td>
+          <td rowspan="3" class="et3" width="201" style="text-align: center">
+            <span class="font2">民俗荟萃舞台</span>
+          </td>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">19:00-19:20</span>
+          </td>
+          <td rowspan="3" class="et3" width="232" style="text-align: center">
+            <span class="font2">佛教文化雕塑南侧</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">20:20-20:40</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">21:40-22:00</span>
+          </td>
+        </tr>
+        <tr>
+          <td rowspan="3" class="et3" width="201" style="text-align: center">
+            <span class="font2">长安故事舞台</span>
+          </td>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">19:40-20:00</span>
+          </td>
+          <td rowspan="3" class="et3" width="232" style="text-align: center">
+            <span class="font2">贞观之治雕塑北侧</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">21:10-21:30</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">22:00-22:20</span>
+          </td>
+        </tr>
+        <tr>
+          <td rowspan="4" class="et3" width="201" style="text-align: center">
+            <span class="font2">追梦力量舞台</span>
+          </td>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">19:00-19:20</span>
+          </td>
+          <td rowspan="4" class="et3" width="232" style="text-align: center">
+            <span class="font2">地屏广场北侧</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">20:10-20:30</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">21:10-21:30</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">22:00-22:20</span>
+          </td>
+        </tr>
+        <tr>
+          <td
+            rowspan="2"
+            class="et10"
+            height="60"
+            width="81"
+            style="text-align: center"
+          >
+            长安里演绎
+          </td>
+          <td rowspan="2" class="et3" width="201" style="text-align: center">
+            <span class="font2">丝路曼舞</span>
+          </td>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">19:00-19:05</span>
+          </td>
+          <td rowspan="2" class="et3" width="232" style="text-align: center">
+            <span class="font2">开元广场东侧</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="et3" width="197" style="text-align: center">
+            <span class="font2">20:10-20:15<br /></span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+<style scoped lang="less">
+* {
+  border-width: 1px;
+  border-style: revert-layer;
+}
+</style>
